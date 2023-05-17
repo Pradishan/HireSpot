3f607789f15e9ae4c87898ae3053b567d4fe54c5
pradishan <k.pradeeshan4@gmail.com>
2023-05-17 12:27:11 +0530

    js ,css added

--- index.php
+++ index.php
@@ -6,13 +6,21 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>HireSpot</title>
+        <!--bootstrap-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+       <!--fontawesom-->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <<link rel="stylesheet" href="maincss.css"/><!--main cs-->
+        
     </head>
     <body>
-
-        <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+        <style>
+            body {
+                background-color: #F1F0F0;
+            }
+        </style>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary " >
             <div class="container">
                 <a class="navbar-brand" href="#"><img src="img/logo.png" width="137px" height="43px" alt="HireSpot"/></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -47,7 +55,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         ?>
         <i class="fa fa-magnifying-glass"></i>
         </div>
-
+        <!--main js-->
+        <<script src="mainjs.js"></script>
+        <!--bootstrap-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </body>
 </html>
