749cea04dda9b70000ba2df2ebdfc2313191c63b
pradishan <k.pradeeshan4@gmail.com>
2023-07-01 00:01:23 +0530

    navigation bar

--- mainjs.js
+++ mainjs.js
@@ -3,4 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
 
+//tool tip
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
