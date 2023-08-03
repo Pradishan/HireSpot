ae37fd7426e7a77a5fc64fe5b76a2ec19ff144d8
Pradisharoon <90965355+Pradishan@users.noreply.github.com>
2023-08-03 11:16:05 +0530

    title name change

--- pages/CompanyPages/posts.php
+++ pages/CompanyPages/posts.php
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
             <meta charset="UTF-8">
             <!-- favicon -->
             <link rel="shortcut icon" href="../../img/logo only.png" type="image/x-icon">
-            <title>HireSpot | Admin </title>
+            <title>HireSpot | Company </title>
             <!--bootstrap-->
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
                 integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
